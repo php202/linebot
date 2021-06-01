7625570828be1790999eac47d0b9d8cd2260bc27
Yutsunghan <php202@gmail.com>
2021-06-01 22:31:32 +0800

    change

--- line/clock.py
+++ line/clock.py
@@ -4,7 +4,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
 def scheduled_job():
-    url = "https://你-APP-的名字.herokuapp.com/"
+    url = "http://34.134.77.7:5000/"
     conn = urllib.request.urlopen(url)
         
     for key, value in conn.getheaders():
